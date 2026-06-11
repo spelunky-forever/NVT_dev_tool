45dcb38c4ab1810cdb9c771ddb97a0cc055921f4
spelunky-forever <haydenpigko@gmail.com>
2026-06-11 08:22:37 -0700

    chore: In qwen3, true replacement is 1/x i.e Reciprocal. To determine error in pyop test, we mock Reciprocal op into directly memcopy from former op

--- pyop_check/node_rsqrt_pyop.py
+++ pyop_check/node_rsqrt_pyop.py
@@ -10,9 +10,13 @@ class RsqrtPyOp:
         buf = out_shapes[0]
         
         try:
+            # 嘗試直接在記憶體中將 C++ 的 buffer 縮放回真正的維度 (例如 3D)
             buf.resize(len(input_shape), refcheck=False)
             buf[:] = input_shape
         except Exception:
+            # 如果 C++ 鎖定 buffer 長度為 4 不給縮放，我們必須「從左邊補 1」
+            # 這樣廣播時尾巴才會對齊！(例如 [1, 512, 1] -> [1, 1, 512, 1])
+            # 絕對不能從右邊補 1，否則會引發 512x512=262144 的廣播災難！
             pad_len = len(buf) - len(input_shape)
             for i in range(len(buf)):
                 if i < pad_len:
@@ -25,7 +29,6 @@ class RsqrtPyOp:
         :param in_data:  list of numpy array (dtype: float32)。包含實際的輸入特徵圖資料。
         :param out_data: list of numpy array (dtype: float32)。C++ 已配置好全 0 的記憶體。
         """
-        x = in_data[0]
-        result = 1.0 / x
-
-        out_data[0][:] = result
+        out_data[0][:] = in_data[0][:]
+        print("Input type: ", in_data[0].flatten()[:5])
+        print("Input value: ", in_data[0].dtype)
